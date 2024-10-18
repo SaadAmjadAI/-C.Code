4c6e9a2445da31267bec887048536149b3b63814
SaadAmjadAI <saadamjadai@gmail.com>
2024-10-18 21:46:18 +0500

    Add files via upload

---
+++ assinment 1st code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main(void)
+{
+int num1=30;
+float num2=80;
+printf ("the integer value of a is %d\n",num1);
+printf("the sum value of a is %f",num2);
+
+getch();
+}
