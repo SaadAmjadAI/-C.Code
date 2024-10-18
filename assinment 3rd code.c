1c6cc31e97784db479d10b47a24ad1cbca771e9b
SaadAmjadAI <saadamjadai@gmail.com>
2024-10-18 22:00:30 +0500

    Add files via upload

---
+++ assinment 3rd code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    float celcius=5,fahrenheit;
+
+    fahrenheit = (celcius * 1.8 ) +32;
+
+    printf("%f celcius = %f fahrenheit",celcius,fahrenheit);
+    
+    getch();
+
+    
+    
+}
